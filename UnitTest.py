7302a69fe6bdb5db5eb06433fba2e3dd59e5fbfc
Gal Cohen <92460450+Gal25@users.noreply.github.com>
2023-05-10 16:39:53 +0300

    more tests

--- UnitTest.py
+++ UnitTest.py
@@ -1,7 +1,9 @@
 import unittest
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QFileDialog, QMessageBox, QDialog
 from front import OptionDialog, InputDialog, InputNum, BertApp
+import os
+from unittest.mock import patch
 
 app = QApplication(sys.argv)
 
@@ -14,26 +16,66 @@ class TestApp(unittest.TestCase):
         dialog.option_combobox.setCurrentIndex(1)
         self.assertEqual(dialog.get_option(), 1)
 
+        dialog.option_combobox.setCurrentIndex(5) #invalid input
+        self.assertEqual(dialog.get_option(), -1)
+
     def test_input_dialog(self):
         dialog = InputDialog()
         dialog.file_path = 'test.txt'
         self.assertEqual(dialog.file_path, 'test.txt')
         dialog.input_field.setText('Test input')
         self.assertEqual(dialog.input_field.text(), 'Test input')
+        dialog.input_field.setText('Gal and Lior are the best')
+        self.assertEqual(dialog.input_field.text(), 'Gal and Lior are the best')
+        dialog.input_field.setText('bert summery')
+        self.assertEqual(dialog.input_field.text(), 'bert summery')
+        dialog.input_field.setText('welcome to our project')
+        self.assertEqual(dialog.input_field.text(), 'welcome to our project')
+
+        dialog.input_field.setText('')
+        with patch.object(QMessageBox, 'exec_', return_value=QMessageBox.Ok):
+            dialog.ok_button.click()
+        self.assertEqual(dialog.result(), QDialog.Rejected)
+
+        app.quit()
 
     def test_input_num_dialog(self):
         dialog = InputNum('Test input', 'ratio')
-        dialog.input_field.setText('5')
-        self.assertEqual(dialog.get_input_text(), '5')
+        dialog.input_field.setText('0.2')
+        self.assertEqual(dialog.get_input_text(), '0.2')
+
+        dialog1 = InputNum('Test input', 'num')
+        dialog1.input_field.setText('7')
+        self.assertEqual(dialog1.get_input_text(), '7')
+
+        dialog2 = InputNum('Test input', 'num')
+        dialog2.input_field.setText('10')
+        self.assertEqual(dialog2.get_input_text(), '10')
+
+        dialog3 = InputNum('Test input', 'ratio')
+        dialog3.input_field.setText('0.6')
+        self.assertEqual(dialog3.get_input_text(), '0.6')
+
 
     def test_bert_app(self):
         app = BertApp('Test summary')
         self.assertEqual(app.result_label.text(), 'Test summary')
 
+        app = BertApp('hello , this is our final project , enjoy!')
+        self.assertEqual(app.result_label.text(), 'hello , this is our final project , enjoy!')
+
     def test_export_result(self):
         app = BertApp('Test summary')
-        app.export_result()  # Simulate clicking the export button
-        # Perform assertion to check if the result is exported successfully
+        temp_file_path = 'test_export.txt' 
+        app.export_result()
+
+        # Check if the file is created and contains the expected content
+        self.assertTrue(os.path.isfile(temp_file_path))
+        with open(temp_file_path, 'r') as file:
+            exported_content = file.read()
+        self.assertEqual(exported_content, app.result_label.text())
+        # Clean up the temporary file
+        os.remove(temp_file_path)
 
     def test_invalid_input_dialog(self):
         dialog = InputDialog()

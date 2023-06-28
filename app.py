57f4847b135562352a196cfdae357f5ede0ab012
Gal Cohen <92460450+Gal25@users.noreply.github.com>
2023-06-28 19:59:41 +0300

    add sendemail

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
 import joblib
 from keybert import KeyBERT
-
+import smtplib
+import webbrowser
 
 
 app = Flask(__name__)
@@ -15,11 +16,8 @@ def hello_world():
 @app.route('/keybert/', methods=['POST'])
 def keyBert():
     kw_model = joblib.load('models/kw_model.sav')
-    # kw_model = KeyBERT()
-
     data = request.form
     text = data['text']
-
     keywords = kw_model.extract_keywords(text, keyphrase_ngram_range=(1, 2), stop_words='english', use_maxsum=True,
                                          nr_candidates=20, top_n=20)
     print("keywords --> \n", keywords)
@@ -30,8 +28,31 @@ def keyBert():
             max = prob
             max_keyword = name
 
-    response = {'result: ': max_keyword}
+    response = {'result:': max_keyword}
     return jsonify(response)
+
+
+@app.route('/sendemail', methods= ['POST'])
+def send_email():
+    data = request.form
+    recipient = data['recipient_email']
+    subject = data['subject']
+    message = data['message']
+
+    try:
+        # Open default email client with pre-populated email
+        mailto_link = f"mailto:{recipient}?subject={subject}&body={message}"
+        webbrowser.open(mailto_link)
+        response = {'result:': 'You can now send your email'}
+        print("Email sent successfully!")
+        return jsonify(response)
+
+    except Exception as e:
+        response = {'result:': 'An error occurred while sending the email'}
+
+        print("An error occurred while sending the email:", str(e))
+        return jsonify(response)
+
 
 
 @app.route('/summarize/ratio', methods=['POST'])
@@ -41,7 +62,7 @@ def sum_ratio():
     ratio = data['ratio']
     text = data['text']
     sum_result = model(text, ratio=float(ratio))
-    response = {'result: ': sum_result}
+    response = {'result:': sum_result}
     return jsonify(response)
 
 
@@ -52,7 +73,7 @@ def sum_number():
     number = data['number']
     text = data['text']
     sum_result = model(text, num_sentences=int(number))
-    response = {'result: ': sum_result}
+    response = {'result:': sum_result}
     return jsonify(response)
 
 if __name__ == '__main__':
